5f7e65585deb768fe668f02eeeac1dd0b7afcf3d
Martin Lundfall <martin.lundfall@protonmail.com>
2018-08-07 17:34:29 +0200

    more ci stuff

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 KLAB_EVMS_PATH=$(pwd)/evm-semantics
+export PATH=$PATH:$(pwd)/bin
 klab server  & cd examples/SafeAdd && klab run --headless --force
+#klab server  & for x in examples/*; do \
+#                   cd $x && klab run --headless --force
+#                   cd ../..
+#               done
 kill $(pgrep klab)
