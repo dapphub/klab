f306e973c43d580a1e0669d06a5824b3b6f5ab73
Martin Lundfall <martin.lundfall@protonmail.com>
2018-11-28 13:57:06 +0100

    build.js, run.sh: feedback fixes

--- resources/run.sh
+++ resources/run.sh
@@ -7,7 +7,7 @@ $KLAB_EVMS_PATH/.build/k/k-distribution/target/release/k/bin/kprove \
 --directory $KLAB_EVMS_PATH/.build/java/ \
 --z3-executable \
 --def-module RULES \
---output-tokenize "#And _==K_ <k> #unsigned <storage>" \
+--output-tokenize "<k> <storage>" \
 --output-omit "<programBytes> <program> <code> <previousGas> <touchedAccounts> <interimStates> <callStack> <callData>" \
 --output-flatten "_Map_ #And" \
 --output json \
