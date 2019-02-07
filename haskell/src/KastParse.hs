25da93471a2af552642f2adac4bac92e99dea36a
Denis <denis.erfurt@gmail.com>
2019-02-07 19:13:48 +0100

    major mainline k rewrk, wip but working version

--- haskell/src/KastParse.hs
+++ haskell/src/KastParse.hs
@@ -5,7 +5,7 @@ import GHC.Generics
 import Data.ByteString.Lazy.UTF8 (fromString)
 import Data.Aeson                (FromJSON, decode)
 import Data.Either               (lefts, rights)
-import Data.List                 (intercalate)
+import Data.List                 (intercalate, isPrefixOf)
 
 import Gas
 
@@ -24,31 +24,39 @@ instance FromJSON Kast
 
 kastToGasExpr :: Kast -> Either String GasExpr
 kastToGasExpr kast = case node kast of
-  "KVariable" -> case name kast of
-    Just "VGas" -> Right $ Nullary StartGas
-    Just somevar -> Left $ "Can't have variables in gas expressions, found: " ++ somevar
-    
+  "KVariable" -> let Just somevar = name kast in
+    if "VGas" `isPrefixOf` somevar
+    then Right $ Nullary StartGas
+    else Left $ "Can't have variables in gas expressions, found: " ++ somevar
+
   "KToken" -> case sort kast of
     Just "Int" -> Right $ Nullary $ Literal n
       where n = read t
             Just t = token kast
     Just somesort -> Left $ "Can't have sorts other than Int, found: " ++ somesort
-    
+
   "KApply" -> case label kast of
+    Just "_+Int_" -> let Just [arg1, arg2] = args kast in
+      case kastToGasExpr arg1 of
+        Left error -> Left error
+        Right e -> case kastToGasExpr arg2 of
+          Left error -> Left error
+          Right f -> Right $ Binary Add e f
+
     Just "_+Int__INT" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
         Left error -> Left error
         Right e -> case kastToGasExpr arg2 of
           Left error -> Left error
           Right f -> Right $ Binary Add e f
-          
+
     Just "_-Int__INT" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
         Left error -> Left error
         Right e -> case kastToGasExpr arg2 of
           Left error -> Left error
           Right f -> Right $ Binary Sub e f
-          
+
     Just "_/Int__INT" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
         Left error -> Left error
@@ -56,7 +64,7 @@ kastToGasExpr kast = case node kast of
           Left error -> Left error
           Right (Nullary (Literal 64)) -> Right $ Unary SixtyFourth e
           Right div -> Left $ "Gas expressions should have /64 only, found: /" ++ (show div)
-          
+
     Just "#if_#then_#else_#fi_K-EQUAL" -> let Just [arg_c, arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
         Left error -> Left error
@@ -66,17 +74,17 @@ kastToGasExpr kast = case node kast of
             Left error -> Left error
             Right c -> Right $ ITE (Cond c) e f
     Just somelabel -> Left $ "Unknown KApply in gas expression: " ++ somelabel
-   
+
 formatKast :: Kast -> Either String String
 formatKast kast = case node kast of
   "KVariable" -> let Just var = name kast in Right var
-  
+
   "KToken" -> case sort kast of
     Just "Int"  -> let Just n = token kast in Right n
     Just "Bool" -> let Just n = token kast in Right n
     Just "K" -> let Just n = token kast in Right n
     Just somesort -> Left $ "Unknown sort: " ++ somesort
-    
+
   "KApply" -> let Just func         = label kast
                   Just apply_args   = args kast
                   lr_fargs = map formatKast apply_args
@@ -104,4 +112,4 @@ formatKApply func fargs = let bracket s = "( " ++ s ++ " )" in
     -- n-ary prefix
     (_, _, _) -> Right $ bracket $ func_trim ++ (bracket (intercalate " , " fargs))
       where func_trim = "`" ++ func ++ "`"
-    
+
--- resources/run_BASE_29483.sh
+++ resources/run_BASE_29483.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 K_OPTS=-Xmx10G \
 $KLAB_EVMS_PATH/.build/k/k-distribution/target/release/k/bin/kprove \
---state-log \
---state-log-path $TMPDIR/klab \
---state-log-id $2 \
---state-log-events OPEN,REACHINIT,REACHTARGET,REACHPROVED,NODE,RULE,SRULE,RULEATTEMPT,IMPLICATION,Z3QUERY,Z3RESULT,CLOSE \
+--debugg \
+--debugg-path $TMPDIR/klab \
+--debugg-id $2 \
 --directory $KLAB_EVMS_PATH/.build/java/ \
+--z3-executable \
 --def-module RULES \
+--output-tokenize "<k> <storage>" \
 --output-omit "<programBytes> <program> <code> <previousGas> <touchedAccounts> <interimStates> <callStack> <callData>" \
 --output-flatten "_Map_ #And" \
 --output json \
