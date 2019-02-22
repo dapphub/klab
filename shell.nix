36c951cd6f55b01dd445f8f31d8b6b5f2aae16e9
Lev Livnev <lev@liv.nev.org.uk>
2019-02-22 09:57:25 +0000

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,27 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "klab-env";
+  buildInputs = [
+    autoconf
+    flex
+    gcc
+    git
+    gmp
+    gnumake
+    maven
+    mpfr
+    nodejs
+    opam
+    openjdk8
+    pandoc
+    pkgconfig
+    python
+    python3
+    z3
+  ];
+  shellHook = ''
+    export PATH=$PWD/bin:$PATH
+    export KLAB_EVMS_PATH=$PWD/evm-semantics
+    export TMPDIR=/tmp/
+  '';
+}
