d4c006d5aa932bb9ce9a600a668e20476db7e0f1
rain <rainbreak@riseup.net>
2019-06-01 10:21:55 +0000

    make compile

--- haskell/klab-build/src/Parser.hs
+++ haskell/klab-build/src/Parser.hs
@@ -1,8 +1,13 @@
-module Parser where
+module Main where
 
 import Data.Char
 import Text.Parsec
 import Text.ParserCombinators.Parsec
+import qualified Options.Applicative as Options
+import System.Exit               (exitWith,
+                                  ExitCode(ExitSuccess),
+                                  ExitCode(ExitFailure))
+import System.IO                 (stderr, hPutStrLn)
 
 data Act = Act
   { contract  :: String
@@ -48,16 +53,19 @@ pAct = do
     spaces
     interface <- many alphaNum
     inputs <- bracketed $ sepBy input (char ',')
-    endBy spaces endOfLine
+    many1 endOfLine
 
     string "forall" <|> string "types"
+    many1 endOfLine
     forall <- withSpaces $ sepBy decl spaces
 
     string "storage"
+    many1 endOfLine
     storage <- withSpaces $ sepBy mapping spaces
 
     string "iff"
-    iffcond <- withSpaces $ sepBy kexp spaces 
+    many1 endOfLine
+    iffcond <- withSpaces $ sepBy kexp spaces
 
     return $ Act
       { contract = contract
@@ -83,16 +91,16 @@ mapping = do
                 datum' <- kexp
                 return $ Rewrite address datum datum'
            , return $ Const address datum ]
- 
-    
+
+
 input :: Parser TypedVar
 input = do
     type' <- withSpaces acttype
-    name <- withSpaces $ many1 alphaNum -- should also allow underscores etc    
+    name <- withSpaces $ many1 alphaNum -- should also allow underscores etc
     return (name, type')
 
 acttype :: Parser Type
-acttype = choice 
+acttype = choice
     [ (string . map toLower . show) t *> pure t | t <- [minBound .. maxBound] ]
 
 
@@ -102,3 +110,48 @@ decl = do
     char ':'
     type' <- withSpaces $ acttype
     return (name, type')
+
+exit    = exitWith ExitSuccess
+die     = exitWith (ExitFailure 1)
+-- input argument is either a path or stdin flag
+data Input
+  = FileInput FilePath
+  | StdInput
+
+fileInput :: Options.Parser Input
+fileInput = FileInput <$> Options.strOption
+  (  Options.long "input"
+  <> Options.short 'i'
+  <> Options.metavar "FILENAME"
+  <> Options.help "Input file" )
+
+stdInput :: Options.Parser Input
+stdInput = Options.flag' StdInput
+  (  Options.long "stdin"
+  <> Options.help "Read from stdin" )
+
+cliInput :: Options.Parser Input
+cliInput = fileInput Options.<|> stdInput
+
+inputToString :: Input -> IO String
+inputToString (FileInput path) = readFile path
+inputToString StdInput = getContents
+
+data ActArgs = ActArgs {
+    act :: Input
+  }
+
+actParser :: Options.Parser ActArgs
+actParser = ActArgs <$> cliInput
+
+main :: IO ()
+main = do
+  let opts = Options.info (actParser Options.<**> Options.helper)
+        (Options.fullDesc
+         <> Options.progDesc ""
+         <> Options.header "")
+  args <- Options.execParser opts
+  s    <- inputToString $ act args
+  case parse pAct "" s of
+    Left e -> putStrLn $ show e
+    Right ast -> putStrLn $ show ast
