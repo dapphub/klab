cd1b8c24cfaf85bd723e9463ff4c04d2d4bcfe87
rain <rainbreak@riseup.net>
2019-06-04 12:44:51 +0000

    gas analyser: fix -Wall warnings

--- haskell/src/KastParse.hs
+++ haskell/src/KastParse.hs
@@ -3,8 +3,7 @@ module KastParse where
 
 import GHC.Generics
 import Control.Lens
-import Data.ByteString.Lazy.UTF8 (fromString)
-import Data.Aeson                (FromJSON, decode)
+import Data.Aeson                (FromJSON)
 import Data.Either               (lefts, rights)
 import Data.List                 (intercalate, isPrefixOf, isSuffixOf)
 
@@ -27,10 +26,11 @@ instance FromJSON Kast
 
 kastToGasExpr :: Kast -> Either String GasExpr
 kastToGasExpr kast = case node kast of
-  "KVariable" -> let Just somevar = originalName kast in
-    if "VGas" `isPrefixOf` somevar
-    then Right $ Nullary StartGas
-    else Left $ "Can't have variables in gas expressions, found: " ++ somevar
+  "KVariable" -> case originalName kast of
+    Just somevar ->
+      if "VGas" `isPrefixOf` somevar
+      then Right $ Nullary StartGas
+      else Left $ "Can't have variables in gas expressions, found: " ++ somevar
 
   "KToken" -> case sort kast of
     Just "Int" -> Right $ Nullary $ Literal n
@@ -41,40 +41,40 @@ kastToGasExpr kast = case node kast of
   "KApply" -> case stripModuleTag <$> (label kast) of
     Just "_+Int_" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
-        Left error -> Left error
+        Left err -> Left err
         Right e -> case kastToGasExpr arg2 of
-          Left error -> Left error
+          Left err -> Left err
           Right f -> Right $ Binary Add e f
 
     Just "_-Int_" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
-        Left error -> Left error
+        Left err -> Left err
         Right e -> case kastToGasExpr arg2 of
-          Left error -> Left error
+          Left err -> Left err
           Right f -> Right $ Binary Sub e f
 
     Just "_*Int_" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
-        Left error -> Left error
+        Left err -> Left err
         Right e -> case kastToGasExpr arg2 of
-          Left error -> Left error
+          Left err -> Left err
           Right f -> Right $ Binary Mul e f
 
     Just "_/Int_" -> let Just [arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
-        Left error -> Left error
+        Left err -> Left err
         Right e -> case kastToGasExpr arg2 of
-          Left error -> Left error
+          Left err -> Left err
           Right (Nullary (Literal 64)) -> Right $ Unary SixtyFourth e
-          Right div -> Left $ "Gas expressions should have /64 only, found: /" ++ (show div)
+          Right n -> Left $ "Gas expressions should have /64 only, found: /" ++ (show n)
 
     Just "#if_#then_#else_#fi" -> let Just [arg_c, arg1, arg2] = args kast in
       case kastToGasExpr arg1 of
-        Left error -> Left error
+        Left err -> Left err
         Right e -> case kastToGasExpr arg2 of
-          Left error -> Left error
+          Left err -> Left err
           Right f -> case formatKast arg_c of
-            Left error -> Left error
+            Left err -> Left err
             Right c -> Right $ ITE (Cond c) e f
     Just somelabel -> Left $ "Unknown KApply in gas expression: " ++ somelabel
 
@@ -94,27 +94,27 @@ formatKast kast = case node kast of
                   fargs       = (^. formula) <$> rights lr_fargs
                   fargs_types = Map.unions $ (^. types) <$> rights lr_fargs
               in case lefts lr_fargs of
-                   error:_ -> Left error
+                   err:_ -> Left err
                    [] -> case formatKApply func fargs of
-                     Left error -> Left error
+                     Left err -> Left err
                      Right x    -> Right $ FormulaicString x fargs_types
 
 formatKApply :: String -> [String] -> Either String String
-formatKApply func fargs = let bracket s = "( " ++ s ++ " )" in
+formatKApply func fargs = let bracketed s = "( " ++ s ++ " )" in
   case (head func, last func, fargs) of
     -- binary infix
-    ('_', '_', (farg1:(farg2:[]))) -> Right $ bracket $ farg1 ++ " " ++ func_trim ++ " " ++ farg2
+    ('_', '_', (farg1:(farg2:[]))) -> Right $ bracketed $ farg1 ++ " " ++ func_trim ++ " " ++ farg2
       where func_trim = tail $ init $ func
     -- unsupported mixfix
     ('_', '_', _) -> Left $ "Couldn't parse mixfix operator: " ++ func
     -- unary prefix
-    (_, '_', (farg:[])) -> Right $ bracket $ func_trim ++ farg
+    (_, '_', (farg:[])) -> Right $ bracketed $ func_trim ++ farg
       where func_trim = init $ func
     -- unary postfix
-    ('_', _, (farg:[])) -> Right $ bracket $ farg ++ func_trim
+    ('_', _, (farg:[])) -> Right $ bracketed $ farg ++ func_trim
       where func_trim = tail $ func
     -- n-ary prefix
-    (_, _, _) -> Right $ bracket $ func_trim ++ (bracket (intercalate " , " fargs))
+    (_, _, _) -> Right $ bracketed $ func_trim ++ (bracketed (intercalate " , " fargs))
       where func_trim = "`" ++ func ++ "`"
 
 -- the module names will be stripped from the labels
