9fb943c5472b019dd38b539b324a6091c12be276
Lev Livnev <lev@liv.nev.org.uk>
2019-06-19 19:17:36 -0400

    klab.nix: bump nixpkgs version (match dapptools)

--- klab.nix
+++ klab.nix
@@ -1,14 +1,9 @@
 let
-  version = "release-19.03";
-  # Import a specific Nixpkgs revision
-  nixpkgs = builtins.fetchTarball {
-    name = "nixpkgs-${version}";
-    # pin the current release-19.03 commit
-    url = "https://github.com/nixos/nixpkgs/archive/50d5d73e22bb2830f490e26a528579facfc7f302.tar.gz";
-    sha256 = "0c1inf0pc2jizkrfl3629s154r55ya5asmwnwn6g64ppz2wwzizi";
+  nixpkgs = builtins.fetchGit {
+    url = "https://github.com/nixos/nixpkgs";
+    ref = "release-19.03";
+    rev = "f1707d8875276cfa110139435a7e8998b4c2a4fd";
   };
   pkgs = import nixpkgs {};
-  compiler = "ghc864";
 in
-with pkgs.haskellPackages;
-pkgs.haskell.packages.${compiler}.callPackage (import ./haskell) {}
+pkgs.haskellPackages.callPackage (import ./haskell) {}
