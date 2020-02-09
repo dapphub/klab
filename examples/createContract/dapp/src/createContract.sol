b5ea140e8f514a4f43ace6a2cff2f310339278d9
dc <dc@log-ix.com>
2020-02-09 18:13:34 +1300

    examples: extend createContract

--- examples/createContract/dapp/src/createContract.sol
+++ examples/createContract/dapp/src/createContract.sol
@@ -9,9 +9,25 @@ contract Kid {
 }
 
 contract Mom {
-  Kid child;
+  Kid kid0;
+  Kid kid1;
+  Kid kid2;
 
-  function create() public {
-    child = new Kid();
+  function create_kid0() public {
+    kid0 = new Kid();
+  }
+
+  function create_kid1() public {
+    bytes memory kidBytecode = type(Kid).creationCode;
+    assembly {
+      sstore(1, create(0, add(kidBytecode, 32), mload(kidBytecode)))
+    }
+  }
+
+  function create_kid2() public {
+    bytes memory kidBytecode = type(Kid).creationCode;
+    assembly {
+      sstore(2, create2(0, add(kidBytecode, 32), mload(kidBytecode), 1))
+    }
   }
 }
--- examples/createContract/src/storage.md
+++ examples/createContract/src/storage.md
@@ -1,8 +1,18 @@
 ### Mom storage
 
 ```k
-syntax Int ::= "#Mom.child" [function]
-rule #Mom.child => 0
+syntax Int ::= "#Mom.kid0" [function]
+rule #Mom.kid0 => 0
+```
+
+```k
+syntax Int ::= "#Mom.kid1" [function]
+rule #Mom.kid1 => 1
+```
+
+```k
+syntax Int ::= "#Mom.kid2" [function]
+rule #Mom.kid2 => 2
 ```
 
 ### Kid storage
