f24f847d4bfa4c496e5f19be633368b3333848e2
dc <dc@log-ix.com>
2020-02-11 12:23:37 +1300

    examples: createContract dapp test

---
+++ examples/createContract/dapp/src/createContract.t.sol
@@ -0,0 +1,28 @@
+pragma solidity >=0.5.15;
+
+import {DSTest} from "src/test.sol";
+import {Mom} from "src/createContract.sol";
+
+contract MomTest is DSTest {
+  Mom mom;
+
+  function setUp() public {
+    mom = new Mom();
+  }
+
+  function test_create_kid0() public {
+    mom.create_kid0();
+    assertEq(mom.kid0(), 0xdf4626ABbA225CdC0ED0F4E57707ee0F54322005);
+  }
+
+  function test_create_kid1() public {
+    mom.create_kid1();
+    assertEq(mom.kid1(), 0xdf4626ABbA225CdC0ED0F4E57707ee0F54322005);
+  }
+
+  function test_create_kid2() public {
+    mom.create_kid2();
+    assertEq(mom.kid2(), 0xDeb0D4a9860A97c02c7642992aec9B895bE89e96);
+  }
+
+}
