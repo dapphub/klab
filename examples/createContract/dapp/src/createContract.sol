852900ef96599c954d8f58f4d7b94b157d27f0b8
dc <dc@log-ix.com>
2020-02-14 17:25:11 +1300

    examples: createContract packed salt

--- examples/createContract/dapp/src/createContract.sol
+++ examples/createContract/dapp/src/createContract.sol
@@ -12,6 +12,7 @@ contract Mom {
   address public kid0;
   address public kid1;
   address public kid2;
+  address public kid3;
 
   function create_kid0() public {
     kid0 = address(new Kid());
@@ -28,6 +29,14 @@ contract Mom {
     bytes memory kidBytecode = type(Kid).creationCode;
     assembly {
       sstore(2, create2(0, add(kidBytecode, 32), mload(kidBytecode), 1))
+    }
+  }
+
+  function create_kid3(address trait1, address trait2) public {
+    bytes memory kidBytecode = type(Kid).creationCode;
+    bytes32 salt = keccak256(abi.encodePacked(trait1, trait2));
+    assembly {
+      sstore(3, create2(0, add(kidBytecode, 32), mload(kidBytecode), salt))
     }
   }
 }
